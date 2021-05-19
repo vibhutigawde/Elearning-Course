e6023d39132bafd6a0d6f07c8d38ccdff9d3f744
vibhutigawde <44055465+vibhutigawde@users.noreply.github.com>
2021-05-19 12:28:19 +0530

    files added

---
+++ Sample 2/story_content/user.js
@@ -0,0 +1,224 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5hfxKvR4apd":
+        Script1();
+        break;
+      case "6mTMLI7R6SQ":
+        Script2();
+        break;
+      case "5unZFnzKrN4":
+        Script3();
+        break;
+      case "5vD0UkTYuew":
+        Script4();
+        break;
+      case "5cd4trTA5FE":
+        Script5();
+        break;
+      case "6c4SdZ3U5g1":
+        Script6();
+        break;
+      case "6FWtcsKxtwc":
+        Script7();
+        break;
+      case "65eRMvEFkxr":
+        Script8();
+        break;
+      case "6DB7FUiCaJZ":
+        Script9();
+        break;
+      case "63nApwBvaod":
+        Script10();
+        break;
+      case "6eh2SI2nSCd":
+        Script11();
+        break;
+      case "6GMBMjDhlEh":
+        Script12();
+        break;
+      case "5nFV8JAUlE0":
+        Script13();
+        break;
+      case "6ivIKdp6hjs":
+        Script14();
+        break;
+      case "5hki3EPAWi0":
+        Script15();
+        break;
+      case "6ix58WxhB4g":
+        Script16();
+        break;
+      case "67Ksdsz6xpS":
+        Script17();
+        break;
+      case "6A0ze21I4Zw":
+        Script18();
+        break;
+      case "6JfZvcAAqLa":
+        Script19();
+        break;
+      case "6cCyUkDrDwA":
+        Script20();
+        break;
+      case "6h36kwTNWOB":
+        Script21();
+        break;
+  }
+}
+
+function Script1()
+{
+  if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{font-family: Arial !important; font-size: 12px !important; margin-bottom: 70px !important}";
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
+function Script2()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0); //mute 
+}
+
+function Script3()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(1); //unmute
+}
+
+function Script4()
+{
+  if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{font-family: Arial !important; font-size: 12px !important; margin-bottom: 70px !important}";
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
+function Script5()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0); //mute 
+}
+
+function Script6()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(1); //unmute
+}
+
+function Script7()
+{
+  if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{font-family: Arial !important; font-size: 12px !important; margin-bottom: 70px !important}";
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
+function Script8()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0); //mute 
+}
+
+function Script9()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(1); //unmute
+}
+
+function Script10()
+{
+  if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{font-family: Arial !important; font-size: 12px !important; margin-bottom: 70px !important}";
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
+function Script11()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0); //mute 
+}
+
+function Script12()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(1); //unmute
+}
+
+function Script13()
+{
+  if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{font-family: Arial !important; font-size: 12px !important; margin-bottom: 70px !important}";
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
+function Script14()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0); //mute 
+}
+
+function Script15()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(1); //unmute
+}
+
+function Script16()
+{
+  if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{font-family: Arial !important; font-size: 12px !important; margin-bottom: 70px !important}";
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
+function Script17()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0); //mute 
+}
+
+function Script18()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(1); //unmute
+}
+
+function Script19()
+{
+  if (!window.movedCaptions) {
+ var sheet = document.createElement('style');
+ sheet.innerHTML = ".caption{font-family: Arial !important; font-size: 12px !important; margin-bottom: 70px !important}";
+ document.body.appendChild(sheet);
+ window.movedCaptions = true;
+}
+}
+
+function Script20()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(0); //mute 
+}
+
+function Script21()
+{
+  var appState = window.DS ? DS.appState : require("helpers/appState");
+appState.onToggleVolume(1); //unmute
+}
+
